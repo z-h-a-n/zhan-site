c3a2561bb9aa28e575aebda559b7bb635c5cc9e2
zhan wang <zhanzw@gmail.com>
2015-06-02 09:13:20 +0100

    added css

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+	def home
+	end
+
+	def cv
+	end
+
+	def work	
+	end
+
+	def lez
+	end
+
+	def sdl
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  get '/home', to: 'home#home'
+  get '/cv', to: 'home#cv'
+  get '/work', to: 'home#work'
+  get '/lez', to: 'home#lez'
+  get '/sdl', to: 'home#sdl'
+
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
